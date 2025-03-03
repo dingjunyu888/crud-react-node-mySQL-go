0bd06a1aa2bf2f5673f03728d444af5cf75aa1de
dingjunyu888 <dingjunyu888@gmail.com>
2025-03-03 03:25:58 -0800

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,10 +5,10 @@ import cors from "cors"
 const app = express();
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,      // Use environment variable for the host
-  user: process.env.DB_USER,      // Use environment variable for the user
-  password: process.env.DB_PASS,  // Use environment variable for the password
-  database: process.env.DB_NAME   // Use environment variable for the database name
+  host: process.env.DB_HOST || "localhost",      // Use environment variable for the host
+  user: process.env.DB_USER || "root",      // Use environment variable for the user
+  password: process.env.DB_PASS || "",  // Use environment variable for the password
+  database: process.env.DB_NAME || "test"  // Use environment variable for the database name
 })
 
 app.use(express.json())//return json data using the api server postman
